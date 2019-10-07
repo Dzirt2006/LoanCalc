e167615b2c9965bf3db08a5921515978c114d1ab
Dzirt2006 <Dzirt-2006@mail.ru>
2019-10-06 20:35:13 -0400

    v1.2

--- Mortgage.cpp
+++ Mortgage.cpp
@@ -5,6 +5,7 @@
 #include "Mortgage.h"
 #include <math.h>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 double Mortgage::CalcTerm(double r,int y)
@@ -39,11 +40,17 @@ double Mortgage::TotalCalc(Vars st){
     double TotalPeriod=st.years*12;
     static double PVL=st.Loan;
     double payment=CalcPayment(st);
-
+    cout << fixed ;
+    cout<<endl;
+    cout<<"Your current loan amount is: "<<setprecision(2)<<st.Loan<<" $"<<endl;
+    cout<<"Loan interest is: "<<st.Rate*100<<" %"<<endl;
+    cout<<"Amount of years is: "<<st.years<<endl<<endl;
+    cout<<"Cycles"<<setw(19)<<"Current loan"<<setw(17)<<"Payment"<<setw(21)<<"Interest"<<endl<<endl;
     do{
 
-        cout<<CalcInterest(PVL,st.Rate)<<' '<<PVL<<endl;
+        cout<<setprecision(0)<<TotalPeriod<<setw(20)<< setprecision(2)<<PVL<<setw(20)<< setprecision(2)<<payment<<setw(20)<<      setprecision(2)<<CalcInterest(PVL,st.Rate)<<endl;
         PVL=PVL-payment;
+
         TotalPeriod--;
     }while(TotalPeriod!=0);
 return 0;
